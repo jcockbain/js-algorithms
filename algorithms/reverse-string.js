e17960b9eab5ea684e8360d0274e609e23f9a972
James Cockbain <james.cockbain@ibm.com>
2019-10-25 22:43:53 +0100

    added 2 more methods for reverse string

--- algorithms/reverse-string.js
+++ algorithms/reverse-string.js
@@ -2,8 +2,20 @@ const reverseString = input =>
   input
     .split("")
     .reverse()
-    .join("")
+    .join("");
 
+const reverseString2 = input => {
+  let result = "";
+  for (let char of input) {
+    result = char + result;
+  }
+  return result;
+};
+
+const reverseString3 = input =>
+  input.split("").reduce((result, char) => char + result);
 module.exports = {
   reverseString,
+  reverseString2,
+  reverseString3,
 };
--- test/reverse-string.test.js
+++ test/reverse-string.test.js
@@ -1,6 +1,10 @@
 const assert = require("assert");
 
-const { reverseString } = require("../algorithms/reverse-string");
+const {
+  reverseString,
+  reverseString2,
+  reverseString3,
+} = require("../algorithms/reverse-string");
 
 describe("reverse string test", function() {
   it("reverses hello world", function() {
@@ -8,5 +12,23 @@ describe("reverse string test", function() {
   });
   it("should return first charachter of the string", function() {
     assert.equal(reverseString("semaJ"), "James");
+  });
+});
+
+describe("reverse string 2 test", function() {
+  it("reverses hello world", function() {
+    assert.equal(reverseString2("Hello World!"), "!dlroW olleH");
+  });
+  it("should return first charachter of the string", function() {
+    assert.equal(reverseString2("semaJ"), "James");
+  });
+});
+
+describe("reverse string 3 test", function() {
+  it("reverses hello world", function() {
+    assert.equal(reverseString3("Hello World!"), "!dlroW olleH");
+  });
+  it("should return first charachter of the string", function() {
+    assert.equal(reverseString3("semaJ"), "James");
   });
 });
