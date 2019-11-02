da5741d06c1d89b7f24dac7fb5e082d705e62fc4
James Cockbain <james.cockbain@ibm.com>
2019-11-02 15:19:24 +0000

    improved efficiency of two-sum

--- algorithms/two-sum.js
+++ algorithms/two-sum.js
@@ -1,10 +1,9 @@
 const twoSum = (list, target) => {
   const pairs = [];
   for (let i = 0; i < list.length; i += 1) {
-    for (let j = 0; j < i; j += 1) {
-      if (list[i] + list[j] === target) {
-        pairs.push([list[i], list[j]]);
-      }
+    const partner = target - list[i];
+    if (list.slice(i + 1).includes(partner)) {
+      pairs.push([list[i], partner]);
     }
   }
   return pairs;
--- test/two-sum.test.js
+++ test/two-sum.test.js
@@ -4,6 +4,6 @@ const { twoSum } = require('../algorithms/two-sum');
 
 describe('Two Sum', () => {
   it('Should implement two sum', () => {
-    assert.deepEqual(twoSum([1, 2, 2, 3, 4], 4), [[2, 2], [3, 1]]);
+    assert.deepEqual(twoSum([1, 2, 2, 3, 4], 4), [[1, 3], [2, 2]]);
   });
 });
